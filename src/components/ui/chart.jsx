199e3ca00ca2d1131302504f12f73a2702119fc6
MukundaSony <123883205+MukundaSony@users.noreply.github.com>
2025-05-19 20:15:49 +0530

    Updates

--- src/components/ui/chart.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const ChartContainer = ({ children, className, config }) => {
-  return (
-    <div className={className}>
-      {children}
-    </div>
-  );
-};
